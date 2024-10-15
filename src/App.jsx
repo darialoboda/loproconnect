59ae51f0539fd80a94233aea29bbf40bdffee0a1
Daria Loboda <xloboda@stuba.sk>
2024-10-15 20:07:15 +0200

    111

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <nav className="navigation">
+      <div className="container">
+        <ul className="navigation__ul">
+          <li className="navigation__li">
+            <a href="" className="navigation__li">Home</a>
+          </li>
+          <li className="navigation__li">
+            <a href="" className="navigation__li">Features</a>
+          </li>
+          <li className="navigation__li">
+            <a href="" className="navigation__li">Pricing</a>
+          </li>
+          <li className="navigation__li">
+            <a href="" className="navigation__li">Blog</a>
+          </li>
+        </ul>
+        <p className="navigation__title-sm">Startup 3</p>
+        <h1 className="navigation__title">Forget About Code</h1>
+        <p className="navigation__info-text">Startup Framework gives you complete freedom over your creative process — you don’t have to think about any technical aspects. There are no limits and absolutely no coding. </p>
+        <a href="" className="navigation__button">Create an Account</a>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
